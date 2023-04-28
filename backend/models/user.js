4d3590b89fcbad39ca499566205f56fcd0eda205
TensaCoder <herschel.menezes00@gmail.com>
2023-04-28 12:09:48 +0530

    User Model created

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,33 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    password: String,
-});
+// const userSchema = new mongoose.Schema({
+//     name: String,
+//     password: String,
+// });
 
-module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
+
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    timestamp:{
+        type: Date,
+        default: Date.now
+    }
+  });
+
+module.exports = mongoose.model("User", UserSchema);
