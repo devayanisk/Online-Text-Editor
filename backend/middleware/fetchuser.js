0dbfc92c45f24b84d428544ceb2be6439ac6185d
TensaCoder <herschel.menezes00@gmail.com>
2023-04-29 16:34:00 +0530

    Document schema created

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const fetchuser = (req, res, next)=>{
     // Get the user id from the jwt token and append it to the next req body
-    const token = req.header('auth-token')
+    const token = req.header('authToken')
     if (!token){
         res.status(401).send({error: "Please authenticate using valid token"})
     }
