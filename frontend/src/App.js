f056685a4212ef101fdf220b23be0bf8971e6532
TensaCoder <herschel.menezes00@gmail.com>
2023-04-30 17:30:30 +0530

    Navbar Component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
 import "quill/dist/quill.snow.css"
 
 import React from "react";
-import {Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import Editor from "./pages/Editor/Editor"
 import Login from "./pages/Authentication/Login";
 import Signup from "./pages/Authentication/Signup";
+import Notes from "./pages/Notes/Notes";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/editor" element={<Editor/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path='/signup' element={<Signup />}/>
-    </Routes>
+    <>
     
+      <Navbar />
+
+      <Routes>
+        <Route exact path="/editor/:id" element={<Editor />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path='/signup' element={<Signup />} />
+        <Route exact path="/notes" element={<Notes />} />
+      </Routes>
+    </>
+
   )
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -19,9 +19,9 @@ const Navbar = () => {
                         <li className="nav-item">
                             <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">Home</Link>
                         </li>
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <Link className={`nav-link ${location.pathname==="/about"? "active": ""}`} to="/about">About</Link>
-                        </li>
+                        </li> */}
                         <li className="nav-item">
                             <Link className={`nav-link ${location.pathname==="/notes"? "active": ""}`} to="/notes">Notes</Link>
                         </li>
